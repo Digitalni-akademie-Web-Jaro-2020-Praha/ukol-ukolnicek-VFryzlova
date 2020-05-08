59abbaca15f897c5e8124876fc2924e738e252fa
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-05-08 13:11:19 +0200

    Zobrazení seznamu úkolů

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
-document.querySelector(".todo__tasks").innerHTML = "";
+
 const tasks = [
   "Zalít květiny",
   "Vyprat prádlo",
   "Vynést koš",
   "Vyluxovat",
 ];
+const updateTasks = () => {
+  const taskElm = document.querySelector(".todo__tasks");
+  taskElm.innerHTML = "";
+  for (let i = 0; i < tasks.length; i ++){
+    taskElm.innerHTML += `<div class="task">${tasks[i]}</div>`;
+  }
+}
+updateTasks();
